d2034c6c1bc5c9d9053fae4768acd25f3b47d330
Kirill Temnenkov <kirill@temnenkov.com>
2022-08-24 17:40:55 +0300

    drawMaze ok with test
    Took 8 minutes

--- src/test/kotlin/com/temnenkov/maze/DrawMazeKtTest.kt
+++ src/test/kotlin/com/temnenkov/maze/DrawMazeKtTest.kt
@@ -7,7 +7,6 @@ internal class DrawMazeKtTest {
     @Test
     fun noPasses() {
         val maze: Maze2D = Maze2DImpl(6, 5)
-        val display = drawMaze(maze)
         assertEquals(
             """+-+-+-+-+-+-+
 | | | | | | |
@@ -21,7 +20,25 @@ internal class DrawMazeKtTest {
 | | | | | | |
 +-+-+-+-+-+-+
 """,
-            display
+            drawMaze(maze)
+        )
+    }
+
+    @Test
+    internal fun twoPasses() {
+        val maze: Maze2D = Maze2DImpl(3, 3)
+        maze.linkToNorth(0, 0)
+        maze.linkToEast(0, 1)
+        assertEquals(
+            """+-+-+-+
+| | | |
++-+-+-+
+|   | |
++ +-+-+
+| | | |
++-+-+-+
+""",
+            drawMaze(maze)
         )
     }
 }
